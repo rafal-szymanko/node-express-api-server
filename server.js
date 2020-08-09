b1e17702dd19c72ff59716119326b232ad04b108
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-09 13:16:56 +0200

    Add frontend files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const db = require('./db');
+const cors = require('cors')
+
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
@@ -12,11 +14,12 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+app.use(cors());
 
 
-app.use('/', testimonialsRoutes); 
-app.use('/', concertRoutes); 
-app.use('/', seatsRoutes); 
+app.use('/api', testimonialsRoutes); 
+app.use('/api', concertRoutes); 
+app.use('/api', seatsRoutes); 
 
 
 app.use((req, res) => {
