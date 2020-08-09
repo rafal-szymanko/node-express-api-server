7924c4f2d4ce1ea3d4788a3fc5428a9f647b79e8
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-09 16:43:04 +0200

    Add middlewares

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const db = require('./db');
 const cors = require('cors')
+const path = require('path');
 
 
 const app = express();
-const port = 8000;
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
@@ -16,10 +16,16 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.use('/api', testimonialsRoutes); 
-app.use('/api', concertRoutes); 
-app.use('/api', seatsRoutes); 
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertRoutes);
+app.use('/api', seatsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 
 app.use((req, res) => {
@@ -28,7 +34,6 @@ app.use((req, res) => {
   });
 })
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Example app listening at http://localhost:8000`);
 });
